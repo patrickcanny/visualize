f7b50e0e5cda888d4e4ca7a5ec7ced6f0361025a
Patrick Canny <patrickcanny@ku.edu>
2018-11-11 21:12:18 -0600

    initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='practice in visualizing data using a variety of visualization libraries',
+    author='patrick canny',
+    license='MIT',
+)
